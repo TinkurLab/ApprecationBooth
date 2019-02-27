289617321fa00c6a7fdd5789fda66aab71b79da6
mgorbsky <gorma09@c02tw107hv2v.ca.com>
2019-02-27 15:51:11 -0700

    this will totally work!

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ TinkurBooth
 Created by Adam Zolyak
 Tinkurlab
 www.TinkurLab.com
+
+
+
+sudo apt-get install graphicsmagick
+
--- boothsnap.py
+++ boothsnap.py
@@ -49,43 +49,44 @@ while True:
 	
 	print "processing photos"
 
-	subprocess.call (["convert", "-delay", "100", "-size", "600x600", "0.jpg", "1.jpg", "2.jpg", "3.jpg", "-loop", "0", "ff.gif"])
+	graphicsmagick = "gm convert -delay 100 *.jpg ff.gif" 
+	subprocess.call (graphicsmagick)
 	
 	print "uploading photos"
 		
 	#send email
-	def sendMail(to, subject, text, files=[]):
-		assert type(to)==list
-		assert type(files)==list
+	# def sendMail(to, subject, text, files=[]):
+	# 	assert type(to)==list
+	# 	assert type(files)==list
 	
-		msg = MIMEMultipart()
-		msg['From'] = USERNAME
-		msg['To'] = COMMASPACE.join(to)
-		msg['Date'] = formatdate(localtime=True)
-		msg['Subject'] = subject
+	# 	msg = MIMEMultipart()
+	# 	msg['From'] = USERNAME
+	# 	msg['To'] = COMMASPACE.join(to)
+	# 	msg['Date'] = formatdate(localtime=True)
+	# 	msg['Subject'] = subject
 		
-		msg.attach( MIMEText(text) )
+	# 	msg.attach( MIMEText(text) )
 	
-		for file in files:
-			part = MIMEBase('application', "octet-stream")
-			part.set_payload( open(file,"rb").read() )
-			Encoders.encode_base64(part)
-			part.add_header('Content-Disposition', 'attachment; filename="%s"'
-							% os.path.basename(file))
-			msg.attach(part)
+	# 	for file in files:
+	# 		part = MIMEBase('application', "octet-stream")
+	# 		part.set_payload( open(file,"rb").read() )
+	# 		Encoders.encode_base64(part)
+	# 		part.add_header('Content-Disposition', 'attachment; filename="%s"'
+	# 						% os.path.basename(file))
+	# 		msg.attach(part)
 	
-		server = smtplib.SMTP('smtp.gmail.com:587')
-		server.ehlo_or_helo_if_needed()
-		server.starttls()
-		server.ehlo_or_helo_if_needed()
-		server.login(USERNAME,PASSWORD)
-		server.sendmail(USERNAME, to, msg.as_string())
-		server.quit()
+	# 	server = smtplib.SMTP('smtp.gmail.com:587')
+	# 	server.ehlo_or_helo_if_needed()
+	# 	server.starttls()
+	# 	server.ehlo_or_helo_if_needed()
+	# 	server.login(USERNAME,PASSWORD)
+	# 	server.sendmail(USERNAME, to, msg.as_string())
+	# 	server.quit()
 	
-	sendMail( ["sender@youremail.com"],
-			"Email Subject Goes Here",
-			"Email Body Goes Here",
-		["ff.gif"])
+	# sendMail( ["sender@youremail.com"],
+	# 		"Email Subject Goes Here",
+	# 		"Email Body Goes Here",
+	# 	["ff.gif"])
 
 
 	print "done - ready to shoot"
