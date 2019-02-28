124f934012e63801cceee24d5d4b07cd645fd1aa
mgorbsky <gorma09@c02tw107hv2v.ca.com>
2019-02-28 15:46:38 -0700

    wip

--- led_test.py
+++ led_test.py
@@ -3,7 +3,7 @@ from luma.core.render import canvas
 from luma.led_matrix.device import max7219
 
 serial = spi(port=0, device=0, gpio=noop())
-device = max7219(serial)
+device = max7219(serial, rotate=1)
 
 with canvas(device) as draw:
    draw.rectangle(device.bounding_box, outline="white", fill="black"),
