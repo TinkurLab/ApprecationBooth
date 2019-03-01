f8aaf5e90d5a674b062d6870749918f364002035
mgorbsky <gorma09@c02tw107hv2v.ca.com>
2019-03-01 09:04:32 -0700

    wrapped 'run' in a method

--- boothsnap.py
+++ boothsnap.py
@@ -64,68 +64,73 @@ show_message(device, msg, fill="white", font=proportional(LCD_FONT), scroll_dela
 print("booth starting up...")
 
 
-
-while True:
-	io.wait_for_edge(pir_pin, io.FALLING)
-	print("PIR ALARM!")
-	
-	#photo capture
-	for x in range(total_dur):
-		file_name = str(x) + '.jpg'
-		print "Taking photo in 3..."
-		sleep (1)
-		print "Taking photo in 2..."
-		sleep (1)
-		print "Taking photo in 1..."
-		sleep (1)
-		subprocess.call (["raspistill", "-o", file_name, "-n", "-w", "600", "-h", "450"])
-		print file_name
-		sleep (fps)
-	
-	print "processing photos"
-
-	graphicsmagick = "gm convert -delay 100 ~/Documents/src/TinkurBooth/*.jpg ~/Documents/src/TinkurBooth/ff.gif" 
-	os.system(graphicsmagick)
-	
-	print "uploading photos"
-
-	
-
-	#send email
-	# def sendMail(to, subject, text, files=[]):
-	# 	assert type(to)==list
-	# 	assert type(files)==list
-	
-	# 	msg = MIMEMultipart()
-	# 	msg['From'] = USERNAME
-	# 	msg['To'] = COMMASPACE.join(to)
-	# 	msg['Date'] = formatdate(localtime=True)
-	# 	msg['Subject'] = subject
+def run
+	while True:
+		io.wait_for_edge(pir_pin, io.FALLING)
+		print("PIR ALARM!")
 		
-	# 	msg.attach( MIMEText(text) )
-	
-	# 	for file in files:
-	# 		part = MIMEBase('application', "octet-stream")
-	# 		part.set_payload( open(file,"rb").read() )
-	# 		Encoders.encode_base64(part)
-	# 		part.add_header('Content-Disposition', 'attachment; filename="%s"'
-	# 						% os.path.basename(file))
-	# 		msg.attach(part)
-	
-	# 	server = smtplib.SMTP('smtp.gmail.com:587')
-	# 	server.ehlo_or_helo_if_needed()
-	# 	server.starttls()
-	# 	server.ehlo_or_helo_if_needed()
-	# 	server.login(USERNAME,PASSWORD)
-	# 	server.sendmail(USERNAME, to, msg.as_string())
-	# 	server.quit()
-	
-	# sendMail( ["sender@youremail.com"],
-	# 		"Email Subject Goes Here",
-	# 		"Email Body Goes Here",
-	# 	["ff.gif"])
+		#photo capture
+		for x in range(total_dur):
+			file_name = str(x) + '.jpg'
+			print "Taking photo in 3..."
+			sleep (1)
+			print "Taking photo in 2..."
+			sleep (1)
+			print "Taking photo in 1..."
+			sleep (1)
+			subprocess.call (["raspistill", "-o", file_name, "-n", "-w", "600", "-h", "450"])
+			print file_name
+			sleep (fps)
+		
+		print "processing photos"
+
+		graphicsmagick = "gm convert -delay 100 ~/Documents/src/TinkurBooth/*.jpg ~/Documents/src/TinkurBooth/ff.gif" 
+		os.system(graphicsmagick)
+		
+		print "uploading photos"
+
+		
+
+		#send email
+		# def sendMail(to, subject, text, files=[]):
+		# 	assert type(to)==list
+		# 	assert type(files)==list
+		
+		# 	msg = MIMEMultipart()
+		# 	msg['From'] = USERNAME
+		# 	msg['To'] = COMMASPACE.join(to)
+		# 	msg['Date'] = formatdate(localtime=True)
+		# 	msg['Subject'] = subject
+			
+		# 	msg.attach( MIMEText(text) )
+		
+		# 	for file in files:
+		# 		part = MIMEBase('application', "octet-stream")
+		# 		part.set_payload( open(file,"rb").read() )
+		# 		Encoders.encode_base64(part)
+		# 		part.add_header('Content-Disposition', 'attachment; filename="%s"'
+		# 						% os.path.basename(file))
+		# 		msg.attach(part)
+		
+		# 	server = smtplib.SMTP('smtp.gmail.com:587')
+		# 	server.ehlo_or_helo_if_needed()
+		# 	server.starttls()
+		# 	server.ehlo_or_helo_if_needed()
+		# 	server.login(USERNAME,PASSWORD)
+		# 	server.sendmail(USERNAME, to, msg.as_string())
+		# 	server.quit()
+		
+		# sendMail( ["sender@youremail.com"],
+		# 		"Email Subject Goes Here",
+		# 		"Email Body Goes Here",
+		# 	["ff.gif"])
 
 
-	print "done - ready to shoot"
+		print "done - ready to shoot"
 
-	time.sleep(1.0)
+		time.sleep(1.0)
+
+try:
+	run()
+except KeyboardInterrupt:
+	pass
