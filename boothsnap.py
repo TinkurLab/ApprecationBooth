061a023e208b1601ccff90f3be31bb1642d6db4c
mgorbsky <gorma09@c02tw107hv2v.ca.com>
2019-03-01 08:56:09 -0700

    mobing working matrix code into boothsnap.py and crossing fingers

--- boothsnap.py
+++ boothsnap.py
@@ -16,8 +16,17 @@ from email.MIMEText import MIMEText
 from email.Utils import COMMASPACE, formatdate
 from email import Encoders
 import os
-import max7219.led as led
 
+# LED Matrix imports
+#import max7219.led as led
+
+from luma.led_matrix.device import max7219
+from luma.core.interface.serial import spi, noop
+from luma.core.render import canvas
+from luma.core.virtual import viewport
+from luma.core.legacy import text, show_message
+from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
+from itertools import repeat
 
 
 ####################################
@@ -37,11 +46,24 @@ USERNAME = "your@emailaddress.com"
 PASSWORD = "yourpassword"
 ####################################
 
-print("booth starting up...")
 
 ### Initialize LED Matrix
-device = led.matrix()
-device.show_message("Hello world!")
+# create device for linear preassembled 4x8x8 max7219 matrix
+serial = spi(port=0, device=0, gpio=noop())
+device = max7219(serial, cascaded=4, block_orientation=-90)
+device.contrast(0x05)
+print("Created device LED Matrix device")
+# note: change device.contrast value (0xXX) to change leds brightness
+
+# start scrolling text demo - just once at startup
+msg = "4Frames Starting Up"
+print(msg)
+show_message(device, msg, fill="white", font=proportional(LCD_FONT), scroll_delay=0.05)
+
+
+print("booth starting up...")
+
+
 
 while True:
 	io.wait_for_edge(pir_pin, io.FALLING)
