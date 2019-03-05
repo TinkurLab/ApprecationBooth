c50e9ee384bcd2cc855bcb3c81e1be98697c493a
Adam Zolyak <adam.zolyak@gmail.com>
2019-03-04 17:56:02 -0700

    wip

--- README.md
+++ README.md
@@ -1,15 +1,18 @@
-TinkurBooth
-===========
+# TinkurBooth
+
 Created by Adam Zolyak
 Tinkurlab
 www.TinkurLab.com
 
-
-
 sudo apt-get install graphicsmagick
 
-pip install dropbox
+pip install python-dotenv
 
 update gpu mem
 https://raspberrypi.stackexchange.com/questions/13764/what-causes-enospc-error-when-using-the-raspberry-pi-camera-module
 
+# config file for variables
+
+FlowdockToken=abcd1234
+PrinterUser=xkcd4321
+PrinterPassword=fafa4343
--- boothsnap.py
+++ boothsnap.py
@@ -1,21 +1,16 @@
 #!/usr/bin/env python
 
-#import for photo capture
+# import for .env variables
+import os
+from dotenv import load_dotenv
+
+# import for photo capture
 import subprocess
 import time
 from time import sleep
 
-#import for motion sensor
+# import for GPIO sensor
 import RPi.GPIO as io
-
-#import for email
-import smtplib
-from email.MIMEMultipart import MIMEMultipart
-from email.MIMEBase import MIMEBase
-from email.MIMEText import MIMEText
-from email.Utils import COMMASPACE, formatdate
-from email import Encoders
-import os
 
 # import for LED Matrix
 #import max7219.led as led
@@ -28,16 +23,23 @@ from luma.core.legacy import text, show_message
 from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
 from itertools import repeat
 
-#import for clock
+# import for clock
 import argparse
 import locale
 import datetime
 import re
 
-#import for IP adress
+# import for IP adress
 import socket
 import fcntl
 import struct
+
+# load .env variables
+load_dotenv(dotenv_path='.env', verbose=True)
+print(os.getenv("FlowdockToken"))
+print(os.getenv("PrinterUser"))
+print(os.getenv("PrinterPassword"))
+
 
 def get_ip_address(ifname):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -47,31 +49,32 @@ def get_ip_address(ifname):
         struct.pack('256s', ifname[:15])
     )[20:24])
 
+
 #print get_ip_address('lo')
 ip = get_ip_address('eth0')
 print ip
 
 ####################################
-#photo capture config
-fps = .1  #delay between photos
-total_dur = 4  #number of photos to be taken
+# photo capture config
+fps = .1  # delay between photos
+total_dur = 4  # number of photos to be taken
 
-#motion sensor config
+# motion sensor config
 button = 18
 
 # GPIO setup
 io.setmode(io.BOARD)
 io.setup(button, io.IN, pull_up_down=io.PUD_UP)
 
-#email config
+# email config
 USERNAME = "your@emailaddress.com"
 PASSWORD = "yourpassword"
 
-locale.setlocale(locale.LC_ALL, '') #use system time
+locale.setlocale(locale.LC_ALL, '')  # use system time
 ####################################
 
 
-### Initialize LED Matrix
+# Initialize LED Matrix
 # create device for linear preassembled 4x8x8 max7219 matrix
 serial = spi(port=0, device=0, gpio=noop())
 device = max7219(serial, cascaded=4, block_orientation=-90)
@@ -79,77 +82,84 @@ device.contrast(0x05)
 print("Created device LED Matrix device")
 # note: change device.contrast value (0xXX) to change leds brightness
 
-#TODO GET THE IP ADDRESS AND DISPLAY ON THE MATRIX SO WE DON'T LOSE ACCESS
+# TODO GET THE IP ADDRESS AND DISPLAY ON THE MATRIX SO WE DON'T LOSE ACCESS
 
 
 print("booth starting up...")
 
+
 def displayScroll(msg):
-	#print(msg)
-	show_message(device, msg, fill="white", font=proportional(LCD_FONT), scroll_delay=0.03)
+    # print(msg)
+    show_message(device, msg, fill="white",
+                 font=proportional(LCD_FONT), scroll_delay=0.03)
+
 
 def displayStatic(msg):
-	#print(msg)
-	with canvas(device) as draw:
-		text(draw, (1, 0), msg, fill="white", font=proportional(LCD_FONT))
+    # print(msg)
+    with canvas(device) as draw:
+        text(draw, (1, 0), msg, fill="white", font=proportional(LCD_FONT))
+
 
 def capture():
-	#photo capture
-	for x in range(total_dur):
-		file_name = str(x) + '.jpg'
-		# sleep (1)
-		# print "Taking photo in 2..."
-		# sleep (1)
-		# print "Taking photo in 1..."
-		# sleep (1)
-		photoCount = x + 1
-		print "Taking photo " + str(photoCount)
-		displayScroll('Photo # ' + str(photoCount) + ' in 3')
-		displayScroll('2')
-		displayScroll('1')
-		displayStatic('Hold!')
-		subprocess.call (["raspistill", "-o", file_name, "-n", "-w", "800", "-h", "600"])
-		print file_name
-		sleep (fps)
-	
-	#print "processing photos"
+    # photo capture
+    for x in range(total_dur):
+        file_name = str(x) + '.jpg'
+        # sleep (1)
+        # print "Taking photo in 2..."
+        # sleep (1)
+        # print "Taking photo in 1..."
+        # sleep (1)
+        photoCount = x + 1
+        print "Taking photo " + str(photoCount)
+        displayScroll('Photo # ' + str(photoCount) + ' in 3')
+        displayScroll('2')
+        displayScroll('1')
+        displayStatic('Hold!')
+        subprocess.call(["raspistill", "-o", file_name,
+                         "-n", "-w", "800", "-h", "600"])
+        print file_name
+        sleep(fps)
 
-	displayScroll('Sending to Cylons')
+    #print "processing photos"
 
-	graphicsmagick = "gm convert -delay 100 ~/Documents/src/TinkurBooth/*.jpg ~/Documents/src/TinkurBooth/ff.gif" 
-	os.system(graphicsmagick)
-	
-	#print "uploading photos"
+    displayScroll('Sending to Cylons')
 
-	displayScroll ('Thank you!')
+    graphicsmagick = "gm convert -delay 100 ~/Documents/src/TinkurBooth/*.jpg ~/Documents/src/TinkurBooth/ff.gif"
+    os.system(graphicsmagick)
+
+    #print "uploading photos"
+
+    displayScroll('Thank you!')
 
 ### MAIN PROGRAM ###
+
+
 def run():
-	io.add_event_detect(button, io.FALLING)  # add falling edge detection on a channel
-	print('listening for button press')
-	sleep(1)
-	
-	while True:
-		#io.wait_for_edge(button, io.FALLING)
-		#print("Someone pushed the button!")
-		
-		if io.event_detected(button):
-			print('Button pressed')
-			## Button pressed, take photos
-			capture()
-			print "done - ready for button press"
+    # add falling edge detection on a channel
+    io.add_event_detect(button, io.FALLING)
+    print('listening for button press')
+    sleep(1)
 
-		#print ('waiting...')
-		time.sleep(1)
-		displayScroll('.')
-		#print('waiting...')
-     	#displayScroll('waiting')
+    while True:
+        #io.wait_for_edge(button, io.FALLING)
+        #print("Someone pushed the button!")
 
+        if io.event_detected(button):
+            print('Button pressed')
+            # Button pressed, take photos
+            capture()
+            print "done - ready for button press"
+
+        #print ('waiting...')
+        time.sleep(1)
+        displayScroll('.')
+        # print('waiting...')
+    # displayScroll('waiting')
 
 
 try:
-	displayScroll('Ready')
-	displayScroll(str(ip))
-	run()
+    displayScroll('Ready')
+    displayScroll(str(ip))
+    run()
 except KeyboardInterrupt:
-	pass
+    pass
